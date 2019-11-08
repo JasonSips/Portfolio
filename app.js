a7a973f92d2f75815627ea54e04b299d8d326311
Jason Cipollari <jasoncipollari@gmail.com>
2019-11-07 21:31:48 -0500

    bringing over the developed version

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
-    $("button").click(function(){
-      $("div").animate({left: '250px'});
-    });
+$("button").click(function(){
+  $("div").animate({
+    left: '250px',
+    opacity: '0.5',
+    height: '150px',
+    width: '150px'
   });
+}); 
